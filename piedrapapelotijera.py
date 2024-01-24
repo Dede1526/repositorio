b8696285632925b6c9c34590692977e87bba3ed0
= <=>
2024-01-24 14:07:04 +0100

    Segunda versión del juego

--- piedrapapelotijera.py
+++ piedrapapelotijera.py
@@ -7,5 +7,11 @@ tirada = input("escoge tu tirada: ")
 
 tiradaordenador = random.randint(1,3)
 
-print(tirada)
-print(tiradaordenador)
+if tiradaordenador == 1:
+    print("El ordenador saca piedra")
+
+elif tiradaordenador == 2:
+    print("El ordenador saca papel")
+
+elif tiradaordenador == 3:
+    print("El ordenador saca tijera")
